ca23b7a05203ad5637f512be90ca0896aaa5fcc6
Marcelo Campos de Medeiros <61807949+marcelocmedeiros@users.noreply.github.com>
2020-06-26 15:53:21 -0300

    add pergunta

--- SequenciasListasTuplasDicionarios/Lista/Exemp017.py
+++ SequenciasListasTuplasDicionarios/Lista/Exemp017.py
@@ -1,13 +1,9 @@
 '''
-Pedimos um inteiro positivo, de quantos elementos o usuário deseja ver.
-Em seguida, criamos a lista, com o valor de 1 dentro, que é quanto vale 0!
-
-Agora, entramos no for. Que vai calcular o valor do termo de índice 1, termo
-de índice 2, termo de índice 3...até o valor de termo n-1.
-
-Para calcular cada novo termo, basta pegar o termo anterior e multiplicar
-pelo índice atual, afinal:
-n! = n * (n-1)!
+2. Faça um script que pede um número inteiro positivo ao usuário.
+Em seguida, cria uma lista com igual número de itens, onde o primeiro termo
+é 1!, o segundo é 2!, o terceiro é o valor de 3!, etc, até o termo que ele
+digitou. Ou seja, se digitou n, vai exibir até o termo de índice n-1, e lá na lista
+vai ter o valor de (n-1)!.
 '''
 
 n = int(input("Gerar sequencia até o termo: "))
